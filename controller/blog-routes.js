419be143f20c725aaacca03703318fdff54bfdd5
kshwi <kshwiss@gmail.com>
2023-05-14 13:50:45 -0500

    added a lot, routes and models about ready, still need to add the blog route and input method

---
+++ controller/blog-routes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { BlogPost } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const BlogData = await BlogPost.findAll({});
+
+    const blog = BlogPost.map(blog => blog.get({ plain: true }));
+
+    res.render("homepage", {
+      blog,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/blogpost/:id", async (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+  } else {
+    try {
+      const BlogData = await BlogPost.findByPk(req.params.id);
+      const blog = BlogData.get({ plain: true });
+      res.render("blogs", { blog, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,46 @@
+const loginFormer = async event => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#pass-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+};
+
+const signupFormer = async event => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signer").value.trim();
+  const email = document.querySelector("#email-signer").value.trim();
+  const password = document.querySelector("#password-signer").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginFormer);
+
+document.querySelector(".signup-form").addEventListener("submit", signupFormer);
