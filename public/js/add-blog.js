d7ee4cd111bf29709294d35cea86338fc11d3c9a
kshwi <kshwiss@gmail.com>
2023-07-03 11:33:16 -0500

    reqorking crud functions, incorporating fetching

--- public/js/add-blog.js
+++ public/js/add-blog.js
@@ -1,11 +1,23 @@
-let text = document.querySelector("#new-post").value;
-let title = document.querySelector("#new-title").value;
-let db = require("./config/connection");
-let email = User.email;
+const addBlog = async event => {
+  event.preventDefault();
+
+  let text = document.querySelector("#new-post").value;
+  let title = document.querySelector("#new-title").value;
+
+  if (text && title) {
+    const response = await fetch("/model/BlogPost", {
+      method: "POST",
+      body: JSON.stringify({ text, title }),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add that blog post.");
+    }
+  }
+};
+
 const BlogSubmit = document.querySelector("#submit-new-blog");
-BlogSubmit.addEventListener(
-  "submit",
-  db.query(
-    `INSERT INTO BlogPost (title, text, email) VALUES (${title},${text},${email}`
-  )
-);
+BlogSubmit.addEventListener("submit", addBlog());
--- public/js/rud.js
+++ public/js/rud.js
@@ -1,19 +1,40 @@
 // import { ConnectionAcquireTimeoutError } from "sequelize";
-let connection = require("./config/connection");
-let Finder = document.querySelector("#find-post").value;
-let Found = document.querySelector("#finding");
+// let connection = require("./config/connection");
+// let Finder = document.querySelector("#find-post").value;
+// let Found = document.querySelector("#finding");
 
-let TempFunc = `SELECT BlogPost FROM Posts WHERE Title =${Finder}`;
+// let TempFunc = `SELECT BlogPost FROM Posts WHERE Title =${Finder}`;
 
-Found.addEventListener("submit", connection.query(TempFunc));
+// Found.addEventListener("submit", connection.query(TempFunc));
 
-let ShowPost = document.querySelector("#found-post");
-// ShowPost.innerHTML= (TempFunc.results);
-let Upper = document.querySelector("#up-post").value;
-let UpText = document.querySelector("#up-post-text").value;
+// let ShowPost = document.querySelector("#found-post");
+// // ShowPost.innerHTML= (TempFunc.results);
+// let Upper = document.querySelector("#up-post").value;
+// let UpText = document.querySelector("#up-post-text").value;
+
+const getBlog = async event => {
+  event.preventDefault();
+
+  let text = document.querySelector("#new-post").value;
+  let title = document.querySelector("#new-title").value;
+
+  if (text && title) {
+    const response = await fetch("/model/BlogPost", {
+      method: "POST",
+      body: JSON.stringify({ text, title }),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add that blog post.");
+    }
+  }
+};
 
 function updateBlogPost() {
-  const query = "UPDATE BlogPost SET text = ? WHERE title = ?";
+  const query = `UPDATE BlogPost SET text = ${UpText} WHERE title = ${Upper}`;
   const values = [blogPost.title, blogPost.text];
   connection.query(query, values, (error, results) => {
     if (error) throw error;
@@ -29,4 +50,24 @@ Founded.addEventListener(
   connection.query(`DELETE FROM Posts WHERE Title =${Nuker}`)
 );
 
+const nukeBlog = async event => {
+  event.preventDefault();
+
+  let text = document.querySelector("#new-post").value;
+  let title = document.querySelector("#new-title").value;
+
+  if (text && title) {
+    const response = await fetch("/model/BlogPost", {
+      method: "POST",
+      body: JSON.stringify({ text, title }),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add that blog post.");
+    }
+  }
+};
 module.exports = { updateBlogPost };
